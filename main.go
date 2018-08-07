ec286e58aad724244fe6cbb768c13e6667c0da1a
nick <nrichu@elon.edu>
2018-08-07 13:38:18 -0400

    fixed logic so coordinates are correct

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"path/filepath"
+	//	"sort"
 
 	"gocv.io/x/gocv"
 	"gocv.io/x/gocv/contrib"
@@ -12,7 +14,7 @@ import (
 
 func main() {
 
-	imageNames := readImagesIn("/home/nick/Documents/PetProjects/PuzzleSolverGo/pieces_aligned")
+	imageNames := readImagesIn("/home/nick/Documents/ComputerVision/hwFour/hawaii/pieces_aligned")
 	guideImage := gocv.IMRead("hawaii_full.jpg", gocv.IMReadAnyColor)
 	imageList := storeImagesInArray(imageNames)
 	puzzleSolverWithoutRotation(guideImage, imageList)
@@ -46,7 +48,7 @@ func storeImagesInArray(imageNames []string) []gocv.Mat {
 		imageList = append(imageList, gocv.IMRead(file, gocv.IMReadAnyColor))
 	}
 
-	return imageList
+	return imageList[:5]
 }
 
 func showImage(windowName string, image gocv.Mat) {
@@ -57,30 +59,41 @@ func showImage(windowName string, image gocv.Mat) {
 
 func puzzleSolverWithoutRotation(guideImage gocv.Mat, imageList []gocv.Mat) {
 	var badImages []gocv.Mat
-	//	var goodMatches []gocv.DMatch
-	var matches [][][]gocv.DMatch
+	var goodMatches []gocv.DMatch
 
 	mask := gocv.NewMat()
 	si := contrib.NewSIFT()
 	bf := gocv.NewBFMatcher()
-
-	_, desc1 := si.DetectAndCompute(guideImage, mask)
-
+	kpGuide, desc1 := si.DetectAndCompute(guideImage, mask)
 	for _, singleImage := range imageList {
 		_, desc2 := si.DetectAndCompute(singleImage, mask)
-		if desc2.Empty() {
+		if !desc2.Empty() {
+			match := bf.KnnMatch(desc1, desc2, 10)
+			goodMatches = goodMatchFilter(match)
+			for _, match := range goodMatches[:1] {
+				currentKP1 := kpGuide[match.QueryIdx]
+				locationx := int(math.RoundToEven(currentKP1.X)/50) * 50
+				locationy := int(math.RoundToEven(currentKP1.Y)/50) * 50
+				print("----------------------------------\n")
+				print(locationx, " coordinate x \n")
+				print(locationy, " coordinate y\n\n")
+
+			}
+
+		} else {
 			badImages = append(badImages, singleImage)
+			print("it hit this")
 		}
-		match := bf.KnnMatch(desc1, desc2, 2)
-		//		for i := 0; i < len(match); i++ {
-		//			if match[i][0].Distance < 0.8*match[i][1].Distance {
-		//				goodMatches = append(goodMatches, match[i][0])
-
-		//			}
-		//		}
-		matches = append(matches, match)
-
 	}
-	print(matches[0])
 
+}
+
+func goodMatchFilter(match [][]gocv.DMatch) []gocv.DMatch {
+	var goodMatches []gocv.DMatch
+	for i := 0; i < len(match); i++ {
+		if match[i][0].Distance < 0.5*match[i][1].Distance {
+			goodMatches = append(goodMatches, match[i][0])
+		}
+	}
+	return goodMatches
 }
